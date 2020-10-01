0252ca4a426b14d550f432a855ac22c28b0f623d
Galang <pekik60@gmail.com>
2020-10-01 21:18:42 +0700

    Adding a view

---
+++ application/views/materi/Golang/deskripsiSingkat.php
@@ -0,0 +1,18 @@
+	<h2>Apa itu web Laravel?</h2>
+	<p>
+		Kalian pasti tahu bahasa pemrograman PHP? Laravel adalah satu-satunya framework yang membantu Anda untuk
+		memaksimalkan penggunaan PHP di dalam proses pengembangan website.
+		PHP menjadi bahasa pemrograman yang sangat dinamis, tapi semenjak adanya Laravel, dia menjadi lebih powerful,
+		cepat, aman, dan simpel. Setiap rilis versi terbaru, Laravel selalu memunculkan teknologi baru di antara
+		framework PHP lainnya.
+		Laravel diluncurkan sejak tahun 2011 dan mengalami pertumbuhan yang cukup eksponensial. Di tahun 2015, Laravel
+		adalah framework yang paling banyak mendapatkan bintang di Github. Sekarang framework ini menjadi salah satu
+		yang populer di dunia, tidak terkecuali di Indonesia.
+		Laravel fokus di bagian end-user, yang berarti fokus pada kejelasan dan kesederhanaan, baik penulisan maupun
+		tampilan, serta menghasilkan fungsionalitas aplikasi web yang bekerja sebagaimana mestinya. Hal ini membuat
+		developer maupun perusahaan menggunakan framework ini untuk membangun apa pun, mulai dari proyek kecil hingga
+		skala perusahaan kelas atas.
+		Laravel mengubah pengembangan website menjadi lebih elegan, ekspresif, dan menyenangkan, sesuai dengan jargonnya
+		“The PHP Framework For Web Artisans”. Selain itu, Laravel juga mempermudah proses pengembangan website dengan
+		bantuan beberapa fitur unggulan, seperti Template Engine, Routing, dan Modularity.
+	</p>
